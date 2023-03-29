27efd081e6a23ec85b5f3ef4acbe6d408b5699b3
Jean-Noel <jean.noel.richert@gmail.com>
2023-03-29 18:58:09 +0200

    Affichage grille multicolore

---
+++ script.js
@@ -0,0 +1,38 @@
+const carre=document.createElement("div");
+carre.classList.add("carre");
+const cadre=document.querySelector("#cadre");
+
+// fonction de création aléatoire d'une couleur:
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+
+  }
+
+let mesDivs=[]
+let count=0;
+
+document.addEventListener("keydown", function(event) {
+   
+    if (event.key == "ArrowDown" && count<=224){
+            let newCarre=carre.cloneNode();
+            newCarre.classList.add(count);
+            newCarre.style.backgroundColor = getRandomColor(); //ajoute un numéro à la div carré
+            mesDivs[count]=newCarre;
+            cadre.appendChild(mesDivs[count]);
+            count ++;
+            console.log(mesDivs);
+            // console.log(count);
+            console.log(cadre);
+        }
+    else if (event.key == "ArrowUp" && count>=0){
+            mesDivs.pop();
+            cadre.removeChild(mesDivs[(mesDivs.length)-1]);
+            console.log(mesDivs);
+            count --;
+    }
+});
